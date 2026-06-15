1a7b702472c213ca0402614a8ef3fe8afe0e8675
hwang67120-bit <hwang67120@gmail.com>
2026-06-15 19:45:58 +0900

    Spring Security

---
+++ src/main/java/org/example/expert/config/JwtAuthenticationFilter.java
@@ -0,0 +1,68 @@
+package org.example.expert.config;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getRequestURI().startsWith("/auth");
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String bearerJwt = request.getHeader("Authorization");
+
+        if (!StringUtils.hasText(bearerJwt)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = jwtUtil.substringToken(bearerJwt);
+        Claims claims = jwtUtil.extractClaims(jwt);
+
+        Long userId = Long.parseLong(claims.getSubject());
+        String email = claims.get("email", String.class);
+        UserRole userRole = UserRole.valueOf(claims.get("userRole", String.class));
+        String nickname = claims.get("nickname", String.class);
+
+        AuthUser authUser = new AuthUser(userId, email, userRole, nickname);
+
+        List<GrantedAuthority> authorities = List.of(
+                new SimpleGrantedAuthority("ROLE_" + userRole.name())
+        );
+
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(authUser, null, authorities);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+    }
+}
